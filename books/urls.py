a9a90317a2ce8c7538e95d14390efd680f24b2ec
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-03 23:20:00 +0600

    in views.py define function for book_list, create, update,delete

--- books/urls.py
+++ books/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from books import views
 
 urlpatterns = [
-    # path('',views.book_list, name = 'book_list'),
-    # path('new/',views.book_create, name = 'book_create'),
-    # path('edit/<int:pk>/',views.book_update, name = 'book_update'),
-    # path('delete/<int:pk>/',views.book_delete, name = 'book_delete'),
+    path('',views.book_list, name = 'book_list'),
+    path('new/',views.book_create, name = 'book_create'),
+    path('edit/<int:pk>/',views.book_update, name = 'book_update'),
+    path('delete/<int:pk>/',views.book_delete, name = 'book_delete'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,HttpResponse
+from .models import Book
 
 # Create your views here.
+
+def book_list(request):
+    return HttpResponse("Book list")
+def book_create(request):
+    return HttpResponse("Create book")
+def book_update(request, pk):
+    return HttpResponse("update book")
+def book_delete(request, pk):
+    return HttpResponse("delete book")
