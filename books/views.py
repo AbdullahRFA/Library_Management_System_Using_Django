21cbfedb20a24aece9549ec328a993b4d8552b6e
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-03 23:35:12 +0600

    added html file for book list, delete, form

--- books/views.py
+++ books/views.py
@@ -4,10 +4,14 @@ from .models import Book
 # Create your views here.
 
 def book_list(request):
-    return HttpResponse("Book list")
+    # return HttpResponse("Book list")
+    return render(request, 'books/book_list.html')
 def book_create(request):
-    return HttpResponse("Create book")
+    # return HttpResponse("Create book")
+    return render(request, 'books/book_form.html')
 def book_update(request, pk):
-    return HttpResponse("update book")
+    # return HttpResponse("update book")
+    return render(request, 'books/book_form.html')
 def book_delete(request, pk):
-    return HttpResponse("delete book")
+    # return HttpResponse("delete book")
+    return render(request, 'books/book_confirm_delete.html')
